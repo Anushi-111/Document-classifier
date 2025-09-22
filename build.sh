9c28bd422d69a65264b4ad33757e0f41f84a5486
Anushi-111 <anushi0901@gmail.com>
2025-09-22 23:32:30 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install system dependencies (Tesseract and Poppler)
+apt-get update && apt-get install -y tesseract-ocr poppler-utils
+
+# Install Python dependencies
+pip install -r requirements.txt
